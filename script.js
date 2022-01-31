207b1b31e4aff60ef58a87c8d47fc1ca7853ac05
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2022-01-31 20:49:44 +0300

    commit1

---
+++ script.js
@@ -0,0 +1,105 @@
+'use strict'
+// Створення масиву
+// let arr = new Array ();
+// let arr = [];
+// let arrOne = [
+//     'Ivanna',
+//     'Anna',
+//     'Oxana',
+// ];
+// let arrTwo = ['Ivanna','Oxana','Anna'];
+// let arrTwo = [
+//     'Ivanna',
+//     {
+//         type:'JS',
+//         age:31,
+//     },
+//     true,
+//     function () {
+//         console.log ('Hello,Ivanna');
+//     }
+// ];
+
+
+// Отримання значень
+
+// let arrOne = [
+//     'December',
+//     'January',
+//     'February',
+// ];
+// console.log (arrOne[0]);
+// console.log (arrOne[1]);
+// console.log (arrOne[2]);
+// console.log (arrOne[5]);
+
+// let arrTwo = [
+//     'Ivanna',
+//     {
+//         type:'JS',
+//         age:31,
+//     },
+//     true,
+//     function () {
+//         console.log ('Hello,Ivanna');
+//     }
+// ]
+// console.log (arrTwo);
+// console.log (arrTwo[0]);
+// console.log (arrTwo[1]);
+// console.log (arrTwo[2]);
+//  arrTwo[3]();
+
+// let matrix = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+// console.log (matrix);
+// console.log (matrix [0][1]);
+
+// Довжина масива
+
+// let arrOne = ['December','January','February'];
+// console.log (arrOne);
+// console.log (arrOne.length);
+
+// Доступ до масиву
+
+// let arr = ['March', 'April', 'May'];
+// console.log (arr);
+
+// let arrNew = arr;
+
+// arrNew.length = 2;
+// console.log (arr);
+
+// arr[0] = 'December';
+// console.log (arr);
+
+// arr[1]='January';
+// console.log (arr);
+
+// arr[2]='February';
+// console.log (arr);
+
+// arr[3]='March';
+// console.log (arr);
+// arr.push('April');
+// arr.push('May');
+// console.log (arr);
+// arr.shift();
+// console.log (arr);
+// arr.shift();
+// console.log (arr);
+// arr.pop();
+// arr.unshift('winter months');
+// console.log (arr);
+
+// Видаляэмо елемент
+
+let arrLast = ['Ivanna', 'Anna','Oxana'];
+arrLast.splice (1,1);
+// 1 - це номер елемента з якого починаємо видаляти, 1 - це кількість елементів які необхідно видалити
+console.log (arrLast);
+
